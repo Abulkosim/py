a4db99fd3dbfb06f3a0bbad22ad671e4418cc971
Abulkosim <abulkasimkattaev@gmail.com>
2025-10-26 22:48:10 +0500

    ts refresher

---
+++ ts-sandbox/refresher.ts
@@ -0,0 +1,26 @@
+interface Todo {
+  title: string;
+  description: string;
+}
+ 
+function updateTodo(todo: Todo, fieldsToUpdate: Partial<Todo>) {
+  return { ...todo, ...fieldsToUpdate };
+}
+ 
+const todo1 = {
+  title: "organize desk",
+  description: "clear clutter",
+};
+ 
+const todo2 = updateTodo(todo1, {
+  description: "throw out trash",
+});
+
+interface Props {
+  a?: number;
+  b?: string;
+}
+ 
+const obj: Props = { a: 5 };
+ 
+const obj2: Required<Props> = { a: 5 };
